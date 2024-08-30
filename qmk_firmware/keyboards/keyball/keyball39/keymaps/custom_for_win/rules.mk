373d3f78876918f987b66fe99103755ba1bb96ce
tkttghn <tiger.takitategohan@gmail.com>
2024-08-30 17:13:29 +0900

    https://github.com/koktoh/jtu_custom_keycodes/tree/master を元に実装したが、機能していない状況。見直しの必要がある。

---
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/custom_for_win/rules.mk
@@ -0,0 +1,11 @@
+# 定義すると、config.hのLED設定部分が有効になる
+#RGBLIGHT_ENABLE = yes
+
+# 定義すると、keymap.cのOLED設定部分が有効になる
+OLED_ENABLE = yes
+
+# VIA(REMAP)の設定に必要な定義
+VIA_ENABLE = yes
+
+# share the grave key (` and ~) with Escape.
+# GRAVE_ESC_ENABLE = no
